028865a053934ba468075b5fc74b38babbeab59c
CoopPenner <Cooper.penner@pennmedicine.upenn.edu>
2025-01-05 15:19:59 -0500

    Revert "Update pathScoreChecker.m"
    This reverts commit d60f0749cb645ebbb46117d72f141036d9a3ed78.

--- pathScoreChecker.m
+++ pathScoreChecker.m
@@ -1,5 +1,65 @@
 %% step 1, parse out all possible combinations to facilitate group creation
 
+addpath('/Users/pennerc/Documents/MATLAB/GPNMB_Global_Analysis/GPNMB-Global-Analysis')
+%reading in path scores and other requisite info
+pathTable=readtable('/Volumes/PC60/InqueryDatasets/allPatients_updated_pathInfo.xlsx');
+allNames=pathTable.Properties.VariableNames;
+globalDx=pathTable.GlobalDx;
+pdDX=pathTable.PDCDx;
+CNDRDX=pathTable.CNDRDx;
+alsDX=pathTable.ALSDx;
+adDX=pathTable.ADCDx;
+snpStat=pathTable.rs199347;
+pathID=pathTable.INDDID;
+
+allNames=allNames(1:270); %removing supplementary SNP's and whatnot
+allNames=allNames(2:end);
+
+%outputting all brain areas
+tauContain=find(contains(allNames, 'Tau'));
+brainAreas=cell(1,length(tauContain));
+    for dd=1:length(tauContain)
+    brainAreas{dd}= allNames{tauContain(dd)}(1:end-3);
+    end
+%removing NeocorticalT because I don't really understand what that means
+brainAreas(strcmp('NeocorticalT',brainAreas))=[];
+
+
+% reading in cog scores
+cogData=readtable('/Volumes/PC60/InqueryDatasets/allCogScoreSlopesMMSE.csv');
+cogID=cogData.ID;
+cogSlope=cogData.cogSlope;
+endScorez=cogData.endScore;
+
+basicData=readtable('/Volumes/PC60/InqueryDatasets/DetailedBasicInfo_allPatients.xlsx');
+
+
+%it's a little strange to initialize things here since I pass the dataTable
+%object to each of the downstream functions, but I feel it helps with
+%readability and fast iteration 
+
+Parkinson= (contains(globalDx, 'Parkinson') | contains(pdDX, 'Parkinson') | contains(CNDRDX, 'Parkinson')    );
+Alzheimer= (contains(globalDx, 'Alzheimer')  );  
+ALS= contains(globalDx, 'Amyotrophic'); %| contains(alsDX, 'Amyotrophic') | contains(CNDRDX, 'Amyotrophic')    );
+DemLewy= (contains(globalDx, 'Dementia with Lewy Bodies')| contains(pdDX, 'Dementia with Lewy Bodies') | contains(CNDRDX, 'Dementia with Lewy Bodies')    );
+MCI= (contains(globalDx, 'Mild cognitive impairment')); 
+corticoBasal= contains(globalDx, 'Corticobasal syndrome'); %| contains(pdDX, 'Corticobasal syndrome') | contains(CNDRDX, 'Corticobasal syndrome')    );
+bvFTD= (contains(globalDx, 'bvFTD-FTLD')); 
+PPA= (contains(globalDx, 'PPA')); 
+supraNuc= (contains(globalDx, 'Progressive supranuclear palsy')| contains(pdDX, 'Progressive supranuclear palsy') | contains(CNDRDX, 'Progressive supranuclear palsy')    );
+neuroPanel= Alzheimer | DemLewy  | Parkinson| corticoBasal ;
+ParkinsonianPanel= Parkinson |corticoBasal |supraNuc |DemLewy;
+ofInterest=ALS|corticoBasal;
+ParkinsonianDem=corticoBasal|DemLewy;
+other= ~neuroPanel;
+HC=(contains(globalDx, 'Normal')); 
+
+
+overGPNMB= contains(snpStat,'TT'); %the major allele
+het=contains(snpStat, 'CT');
+underGPNMB=contains(snpStat,'CC'); %the minor allele
+
+
 %% quick testarooooo
 
 
